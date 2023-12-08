fb48f1dff27cce2257a14841ba019b26a5a759dc
kajalasodiya47 <140589136+kajalasodiya47@users.noreply.github.com>
2023-12-08 17:07:51 +0530

    Add files via upload

---
+++ task/nested_list_game.py
@@ -0,0 +1,40 @@
+'''
+  l1 = [12,45,343,545,5,6,4,7]
+
+
+  lucky : 5
+
+  computer = [12,45,343,545]
+  user = [5,6,4,7]
+
+  round : 1
+  user win
+  u_score = 1
+  c_score = 0
+
+'''
+
+import random
+l1 = [12,45,343,545,5,6,4,7]
+computer = [12,45,343,545]
+user = [5,6,4,7]
+i = 1
+while i < 5:
+ x = random.choice(l1)
+ print("lucky : ",x)
+ if(x in computer):
+    computer.remove(x)
+    print("round : ",i)
+    print("computer win")
+    print("c_score = ",i)
+    if computer == []:
+       break
+ elif(x in user):
+    user.remove(x)
+    print("round : ",i)
+    print("user win")
+    print("u_score = ",i)
+    if user == []:
+        break    
+    i += 1
+
