01090fd8c2cb26fafb2b4a60de42f7c39ef441c6
kajalasodiya47 <140589136+kajalasodiya47@users.noreply.github.com>
2023-12-08 17:11:24 +0530

    Add files via upload

---
+++ task/dict1.py
@@ -0,0 +1,8 @@
+dict1 = {}
+Rollno=int(input("Enter roll no : "))
+name=input("Enter name : ")
+subject=input("Enter subject : ")
+dict1['rollno']=Rollno
+dict1['name']=name
+dict1['subject']=subject
+print(dict1)
