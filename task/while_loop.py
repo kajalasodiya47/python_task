d27b5894c5ed85f05c5f90730cc64488a063e1d0
kajalasodiya47 <140589136+kajalasodiya47@users.noreply.github.com>
2023-12-08 17:12:53 +0530

    Add files via upload

---
+++ task/while_loop.py
@@ -0,0 +1,20 @@
+   '''
+    while : Entry Control Loop
+
+    Execution control loop
+
+
+    syntax:
+
+    initialization
+    while(condition):.
+         code....
+         updation
+         
+'''
+
+a=1
+while(a<=10):
+    print("Hello")
+    a+=1
+    
