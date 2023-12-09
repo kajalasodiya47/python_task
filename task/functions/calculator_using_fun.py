6ff4f92a03b7942c3e7003aa85ee124be7032f56
kajalasodiya47 <140589136+kajalasodiya47@users.noreply.github.com>
2023-12-09 15:49:52 +0530

    Add files via upload

---
+++ task/functions/calculator_using_fun.py
@@ -0,0 +1,31 @@
+def add(x,y):
+  return x + y
+
+def sub(x,y):
+    return  x - y
+
+def mul(x,y):
+    return x * y
+
+def div(x,y):
+    return  x/y
+
+num1 = int(input("Enter 1st number for addition: "))
+num2 = int(input("Enter 1st number for addition: "))
+print("Addition is : ",add(num1,num2))
+num1 = int(input("Enter 1st number for subtraction: "))
+num2 = int(input("Enter 1st number for subtraction: "))
+print("Subtraction is : ",sub(num1,num2))
+num1 = int(input("Enter 1st number for multiplication: "))
+num2 = int(input("Enter 1st number for multiplication: "))
+print("Multiplication is : ",mul(num1,num2))
+num1 = int(input("Enter 1st number for division: "))
+num2 = int(input("Enter 1st number for division: "))
+print("Division is : ",div(num1,num2))
+
+
+
+
+
+
+
