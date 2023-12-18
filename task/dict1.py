bd8b5f5554d153d3bc64b622af695c8c35db5518
kajalasodiya47 <140589136+kajalasodiya47@users.noreply.github.com>
2023-12-18 17:50:53 +0530

    Delete task/dict1.py

--- task/dict1.py
+++
@@ -1,8 +0,0 @@
-dict1 = {}
-Rollno=int(input("Enter roll no : "))
-name=input("Enter name : ")
-subject=input("Enter subject : ")
-dict1['rollno']=Rollno
-dict1['name']=name
-dict1['subject']=subject
-print(dict1)
